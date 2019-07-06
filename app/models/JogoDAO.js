ab38b27ab44c063e4162607a1979df09340f32c2
Hernany <contatohernany@gmail.com>
2019-07-06 03:02:52 -0300

    Criei a função de iniciar jogo, onde a mesma renderiza a view jogo com os parametros de habilidades que estao no bando de dados

--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -20,7 +20,21 @@ JogoDAO.prototype.gerarParametros = function(usuario){
     });
 }
 
+JogoDAO.prototype.iniciaJogo = function(res, usuario, casa){
+    this._connection.open(function(err, mongoclient){
+        mongoclient.collection('jogo', function(err, collection){
+            collection.find({usuario: usuario}).toArray(function(err, result){
+                
+                console.log(result[0]);
 
+                res.render('jogo', {img_casa: casa, jogo: result[0]});
+
+                mongoclient.close();
+            });          
+        });
+    });
+
+};
 
 
 
